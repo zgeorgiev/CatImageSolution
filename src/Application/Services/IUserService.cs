4e9bb9fd30ed20ca4cfe2b204f30624f661156f1
Zhivko Georgiev <zhivko.georgiev@gmail.com>
2021-06-11 00:13:30 +0300

    Add project files.

---
+++ src/Application/Services/IUserService.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Services
+{
+    public interface IUserService
+    {
+        User Register(User user);
+        User Authenticate(string username, string password);
+    }
+}
